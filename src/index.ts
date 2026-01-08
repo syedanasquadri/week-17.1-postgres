9dbbba9d04af38a4a2000c386e28d9ba24feb3fc
syedanasquadri <syedanasquadri123@gmail.com>
2026-01-08 11:16:31 +0530

    add signup endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
 import {Client} from "pg"
 import { ConnectionString } from "./config";
+import express from 'express'
+
+const app = express();
+app.use(express.json())
 
 const pgClient = new Client(ConnectionString)
 
-async function main(){
-    await pgClient.connect();
-    const response = await pgClient.query("UPDATE users SET username='harkirat' WHERE id=2");
-    console.log(response.rows);
-}
+pgClient.connect();
 
-main();
+app.post("/signup", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+
+    try {
+        const insertQuery = `INSERT INTO USERS(username, password, email) VALUES ($1, $2, $3);`
+
+        const response = await pgClient.query(insertQuery, [username, password, email]);
+
+         res.json({
+             message: "You have signed up"
+        })
+    } catch(e) {
+        console.log(e);
+        res.json({
+            message: "Error while signing up"
+        })
+    }
+})
+
+app.listen(3000);
