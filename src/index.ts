92434c087858c45f45619f23f904fe2c14f245ca
syedanasquadri <syedanasquadri123@gmail.com>
2026-01-09 00:33:23 +0530

    add address field

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,20 @@ app.post("/signup", async (req, res) => {
     const password = req.body.password;
     const email = req.body.email;
 
+    const city = req.body.street;
+    const country = req.body.country;
+    const street = req.body.street;
+    const pincode = req.body.email;
+
     try {
-        const insertQuery = `INSERT INTO USERS(username, password, email) VALUES ($1, $2, $3);`
+        const insertQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING id;`
 
         const response = await pgClient.query(insertQuery, [username, password, email]);
+        const userId = response.rows[0].id;
+
+        const addressInsertQuery = `INSERT INTO addresses (city, country, street, pincode, user_id) VALUES ($1, $2, $3, $4, $5);`
+
+        const responseAddressQuery = await pgClient.query(addressInsertQuery, [city, country, street, pincode, userId]);
 
          res.json({
              message: "You have signed up"
