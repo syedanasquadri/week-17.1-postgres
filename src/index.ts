7f0fdc7a924560c9dd6608933f25a87f64c350d2
syedanasquadri <syedanasquadri123@gmail.com>
2026-01-09 16:49:56 +0530

    add metadata endpoint

--- src/index.ts
+++ src/index.ts
@@ -20,14 +20,16 @@ app.post("/signup", async (req, res) => {
     const pincode = req.body.email;
 
     try {
+        await pgClient.query('BEGIN;')
         const insertQuery = `INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING id;`
+        const addressInsertQuery = `INSERT INTO addresses (city, country, street, pincode, user_id) VALUES ($1, $2, $3, $4, $5);`
 
         const response = await pgClient.query(insertQuery, [username, password, email]);
         const userId = response.rows[0].id;
-
-        const addressInsertQuery = `INSERT INTO addresses (city, country, street, pincode, user_id) VALUES ($1, $2, $3, $4, $5);`
-
+        await new Promise(x => setTimeout(x, 100 * 1000))
         const responseAddressQuery = await pgClient.query(addressInsertQuery, [city, country, street, pincode, userId]);
+
+        await pgClient.query('COMMIT;')
 
          res.json({
              message: "You have signed up"
@@ -38,6 +40,20 @@ app.post("/signup", async (req, res) => {
             message: "Error while signing up"
         })
     }
+})
+
+app.get("/metadata", async (req, res) => {
+    const id = req.query.id;
+    const query1 = `SELECT * FROM users WHERE id=$1;`;
+    const response1 = await pgClient.query(query1,[id]);
+
+    const query2 = `SELECT * FROM addresses WHERE id=$1;`;
+    const response2 = await pgClient.query(query2,[id]);
+
+    res.json({
+        user: response1.rows[0],
+        address : response2
+    })
 })
 
 app.listen(3000);
